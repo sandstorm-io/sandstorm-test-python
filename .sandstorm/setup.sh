5c350b792d974c6eb30e51f54d38d5a2d471c7d2
Drew Fisher <drew@sandstorm.io>
2015-12-04 11:19:32 -0800

    Minimal flask app with ids and JS to do useful things
    * shows app version
    * shows a user-settable piece of state
    * shows Sandstorm header values
    * has several forms for triggering postMessage interactions

---
+++ .sandstorm/setup.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+set -euo pipefail
+# This script is run in the VM once when you first run `vagrant-spk up`.  It is
+# useful for installing system-global dependencies.  It is run exactly once
+# over the lifetime of the VM.
+#
+# This is the ideal place to do things like:
+#
+#    export DEBIAN_FRONTEND=noninteractive
+#    apt-get install -y nginx nodejs nodejs-legacy python2.7 mysql-server
+#
+# If the packages you're installing here need some configuration adjustments,
+# this is also a good place to do that:
+#
+#    sed --in-place='' \
+#            --expression 's/^user www-data/#user www-data/' \
+#            --expression 's#^pid /run/nginx.pid#pid /var/run/nginx.pid#' \
+#            --expression 's/^\s*error_log.*/error_log stderr;/' \
+#            --expression 's/^\s*access_log.*/access_log off;/' \
+#            /etc/nginx/nginx.conf
+
+# By default, this script does nothing.  You'll have to modify it as
+# appropriate for your application.
+exit 0
+
+# Install sandstorm and pycapnp dependencies from apt repos.
+export DEBIAN_FRONTEND=noninteractive
+apt-get install -y build-essential libcap-dev xz-utils zip unzip imagemagick \
+    strace curl clang discount git autoconf pkg-config libtool \
+    python3 python3-pip python3-dev nginx
+
+# Configure nginx for static stuff
+[ -f /etc/nginx/sites-enabled/default ] && unlink /etc/nginx/sites-enabled/default
+cat > /etc/nginx/sites-available/sandstorm-static <<EOF
+server {
+    listen 8000 default_server;
+    listen [::]:8000 default_server ipv6only=on;
+    client_max_body_size 0;
+    server_name localhost
+    root /opt/app;
+}
+EOF
+ln -s /etc/nginx/sites-available/sandstorm-static /etc/nginx/sites-enabled/sandstorm-static
+# patch nginx conf to not bother trying to setuid, since we're not root
+# also patch errors to go to stderr, and logs nowhere.
+sed --in-place='' \
+        --expression 's/^user www-data/#user www-data/' \
+        --expression 's#^pid /run/nginx.pid#pid /var/run/nginx.pid#' \
+        --expression 's/^\s*error_log.*/error_log stderr;/' \
+        --expression 's/^\s*access_log.*/access_log off;/' \
+        /etc/nginx/nginx.conf
+service nginx stop
+systemctl disable nginx
+
+# Install meteor as vagrant user
+su -c "curl https://install.meteor.com/ | sh" vagrant
+
+# Install capnproto from source, since Sandstorm currently depends on unreleased capnproto features.
+if [ ! -e /usr/local/bin/capnp ]; then
+    [ -d capnproto ] || git clone https://github.com/sandstorm-io/capnproto
+    pushd capnproto/c++
+    autoreconf -i && ./configure && make -j2 && sudo make install
+    popd
+fi
+
+# Install pycapnp from PyPI, which should use the system libcapnp we just installed
+pip3 install pycapnp
---
+++ main.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import sys
+
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask import make_response
+import capnp
+
+# Hack to make RPC work in Flask worker threads
+capnp.remove_event_loop()
+capnp.create_event_loop(threaded=True)
+
+# Load the relevant interface descriptors from the current sandstorm bundle.
+bridge = capnp.load("/opt/sandstorm/latest/usr/include/sandstorm/sandstorm-http-bridge.capnp",
+            imports=[
+                "/opt/sandstorm/latest/usr/include",
+            ]
+        )
+hack_session = capnp.load("/opt/sandstorm/latest/usr/include/sandstorm/hack-session.capnp",
+            imports=[
+                "/opt/sandstorm/latest/usr/include",
+            ]
+        )
+
+pkgdef = capnp.load("/sandstorm-pkgdef.capnp",
+            imports=[
+                "/opt/sandstorm/latest/usr/include",
+            ]
+        )
+
+CODE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+app = Flask(__name__)
+
+
+def get_bridge_cap_promise():
+    # Connect to the socket exposed by sandstorm-http-bridge
+    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    sock.connect("/tmp/sandstorm-api")
+    client = capnp.TwoPartyClient(sock)
+    bridge_cap_promise = client.bootstrap().cast_as(bridge.SandstormHttpBridge)
+    return bridge_cap_promise
+
+def write_state(newState):
+    with open("/var/state", "wb") as f:
+        f.write(newState)
+
+def read_state():
+    with open("/var/state") as f:
+        return f.read()
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        newState = request.form.get("state", "").encode('utf-8')
+        write_state(newState)
+    content = read_state()
+    # Load app version from sandstorm-pkgdef.capnp
+    manifest = pkgdef.pkgdef.manifest
+    return render_template("index.html",
+            content=content,
+            session_id=request.headers.get("X-Sandstorm-Session-Id", ""),
+            user_id=request.headers.get("X-Sandstorm-User-Id", ""),
+            username=request.headers.get("X-Sandstorm-Username", ""),
+            handle=request.headers.get("X-Sandstorm-Preferred-Handle", ""),
+            permissions=request.headers.get("X-Sandstorm-Permissions", ""),
+            pronouns=request.headers.get("X-Sandstorm-User-Pronouns", ""),
+            app_version=manifest.appVersion,
+            )
+
+@app.route('/reflect')
+def reflect():
+    # Reflects the headers from a request as a JSON object.
+    # Useful for seeing what permissions/handle/etc were seen by the app after e.g. changing handle,
+    # pronouns, switching identity, etc.
+    headers = {}
+    for (key, value) in request.headers:
+        if not key in headers:
+            headers[key] = []
+        headers[key].append(value)
+    # Future: reflect the POST body? path info? query string? anything else useful?
+    reply = json.dumps({"headers": headers}, sort_keys=True)
+    return make_response(reply, 200)
+
+@app.route('/api')
+def api():
+    body = json.dumps({"state": read_state()})
+    return make_response(body, 200)
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', 8000, debug=True)
