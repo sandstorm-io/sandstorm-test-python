7d0e755cdcf8798cf7edaffbfea1e6441f704b57
Drew Fisher <drew@sandstorm.io>
2016-02-02 17:27:03 -0800

    Powerbox request/offer of SealedUiViews

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+import base64
 import json
 import os
+import socket
 import sys
 
 from flask import Flask
@@ -20,7 +22,25 @@ bridge = capnp.load("/opt/sandstorm/latest/usr/include/sandstorm/sandstorm-http-
                 "/opt/sandstorm/latest/usr/include",
             ]
         )
+util = capnp.load("/opt/sandstorm/latest/usr/include/sandstorm/util.capnp",
+            imports=[
+                "/opt/sandstorm/latest/usr/include",
+            ]
+        )
+
+grain = capnp.load("/opt/sandstorm/latest/usr/include/sandstorm/grain.capnp",
+            imports=[
+                "/opt/sandstorm/latest/usr/include",
+            ]
+        )
+
 hack_session = capnp.load("/opt/sandstorm/latest/usr/include/sandstorm/hack-session.capnp",
+            imports=[
+                "/opt/sandstorm/latest/usr/include",
+            ]
+        )
+
+sealed = capnp.load("/opt/sandstorm/latest/usr/include/sandstorm/sealed.capnp",
             imports=[
                 "/opt/sandstorm/latest/usr/include",
             ]
@@ -35,13 +55,15 @@ CODE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 app = Flask(__name__)
 
-def get_bridge_cap_promise():
+caps_file = '/var/cap-info.json'
+
+def get_bridge_cap():
     # Connect to the socket exposed by sandstorm-http-bridge
     sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     sock.connect("/tmp/sandstorm-api")
     client = capnp.TwoPartyClient(sock)
-    bridge_cap_promise = client.bootstrap().cast_as(bridge.SandstormHttpBridge)
-    return bridge_cap_promise
+    bridge_cap = client.bootstrap().cast_as(bridge.SandstormHttpBridge)
+    return bridge_cap
 
 def write_state(newState):
     with open("/var/state", "wb") as f:
@@ -53,6 +75,8 @@ def read_state():
 
 @app.route('/', methods=["GET", "POST"])
 def index():
+    print(grain.PowerboxDescriptor)
+    sys.stdout.flush()
     if request.method == "POST":
         newState = request.form.get("state", "").encode('utf-8')
         write_state(newState)
@@ -68,6 +92,7 @@ def index():
             permissions=request.headers.get("X-Sandstorm-Permissions", ""),
             pronouns=request.headers.get("X-Sandstorm-User-Pronouns", ""),
             app_version=manifest.appVersion,
+            saved_caps=get_saved_caps(),
             )
 
 @app.route('/reflect')
@@ -83,6 +108,75 @@ def reflect():
     # Future: reflect the POST body? path info? query string? anything else useful?
     reply = json.dumps({"headers": headers}, sort_keys=True)
     return make_response(reply, 200)
+
+def get_saved_caps():
+    if os.path.exists(caps_file):
+        with open(caps_file) as f:
+            return json.load(f)
+    return []
+
+@app.route('/caps', methods=['POST'])
+def savecap():
+    """
+    Save a sturdyref from the client in durable storage.
+
+    Arguably, we should save the PowerboxDescriptor from the client here too,
+    and pass that on when we later do an offer_cap(), rather than forging a
+    new PowerboxDescriptor from whole cloth.
+    """
+    token = request.form.get('token')
+    print("should save", token)
+    sys.stdout.flush()
+    caps = get_saved_caps()
+    caps.append({"token": token})
+    with open(caps_file, "wb") as f:
+        contents = json.dumps(caps)
+        f.write(contents.encode('utf-8'))
+    return make_response("", 200)
+
+@app.route('/caps/<cap_id>', methods=['POST'])
+def offer_cap(cap_id):
+    print("should offer", cap_id)
+    sys.stdout.flush()
+    # Restore the sturdyref into a liveref
+    bridge_cap = get_bridge_cap()
+    liveref_promise = bridge_cap.getSandstormApi().then(
+        lambda res: res.api.cast_as(grain.SandstormApi).restore(cap_id)
+    )
+
+    # Then, offer that liveref to the requesting user's session context
+    session_id = request.headers["X-Sandstorm-Session-Id"]
+    session_ctx_promise = bridge_cap.getSessionContext(session_id)
+
+    def offerCap(res):
+        session_ctx_resp, liveref_resp = res
+        session_ctx = session_ctx_resp.context.cast_as(hack_session.HackSessionContext)
+        liveref = liveref_resp.cap
+        sealedUiViewTag = grain.PowerboxDescriptor.Tag.new_message(id=sealed.SealedUiView.schema.node.id)
+        descriptor = grain.PowerboxDescriptor.new_message(tags=[sealedUiViewTag])
+        displayInfo = grain.PowerboxDisplayInfo.new_message(
+            title=util.LocalizedText.new_message(defaultText="some title"),
+            verbPhrase=util.LocalizedText.new_message(defaultText="some verbPhrase"),
+            description=util.LocalizedText.new_message(defaultText="some description"),
+        )
+        # PowerboxDescriptor is:
+        # tags: List(Tag)
+        #   tag is:
+        #     id uint64 (capnproto type id of an interface or capnproto type id of a struct type)
+        #     value AnyPointer (optional, if struct type id above, an instance of that struct)
+
+        # PowerboxDisplayInfo is
+        # title
+        # verbPhrase
+        # description
+        # all three are Util.LocalizedText
+        return session_ctx.offer(cap=liveref, descriptor=descriptor, displayInfo=displayInfo)
+
+    capnp.join_promises([session_ctx_promise, liveref_promise]).then(
+        offerCap
+    ).wait()
+
+    return make_response("", 200)
 
 @app.route('/api')
 def api():
